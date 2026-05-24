6b71af8af4091f793811ca1cdcb7f29b75ff094e
Naoto Kaneko <naoty.k@gmail.com>
2026-05-24 16:52:32 +0900

    format違反を修正する

--- packages/engine/src/types.ts
+++ packages/engine/src/types.ts
@@ -1,11 +1,11 @@
 /**
  * 局面
  */
-type Position = {
+type _Position = {
   board: Board;
   hands: Hands;
   turn: Color;
-}
+};
 
 /**
  * 盤
@@ -28,7 +28,7 @@ type Hand = Record<UnpromotedPieceType, number>;
 type Square = {
   column: Column;
   row: Row;
-}
+};
 
 /**
  * マスのキー
@@ -51,11 +51,11 @@ type Row = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 type Piece = {
   color: Color;
   type: PieceType;
-}
+};
 
 /**
  * 先後
- * 
+ *
  * - black: 先手
  * - white: 後手
  */
@@ -70,30 +70,30 @@ type PieceType = UnpromotedPieceType | PromotedPieceType;
  * 通常の駒の種類
  */
 type UnpromotedPieceType =
-  | "king"    // 王/玉
-  | "rook"    // 飛
-  | "bishop"  // 角
-  | "gold"    // 金
-  | "silver"  // 銀
-  | "knight"  // 桂
-  | "lance"   // 香
-  | "pawn";   // 歩
+  | "king" // 王/玉
+  | "rook" // 飛
+  | "bishop" // 角
+  | "gold" // 金
+  | "silver" // 銀
+  | "knight" // 桂
+  | "lance" // 香
+  | "pawn"; // 歩
 
 /**
  * 成駒の種類
  */
 type PromotedPieceType =
-  | "rook+"   // 龍
+  | "rook+" // 龍
   | "bishop+" // 馬
   | "silver+" // 成銀
   | "knight+" // 成桂
-  | "lance+"  // 成香
-  | "pawn+";  // と
+  | "lance+" // 成香
+  | "pawn+"; // と
 
 /**
  * 手
  */
-type Move = NormalMove | DropMove;
+type _Move = NormalMove | DropMove;
 
 /**
  * 指し手
@@ -103,7 +103,7 @@ type NormalMove = {
   from: Square;
   to: Square;
   promote: boolean;
-}
+};
 
 /**
  * 持ち駒を打つ手
@@ -112,4 +112,4 @@ type DropMove = {
   type: "drop";
   piece: UnpromotedPieceType;
   to: Square;
-}
+};
