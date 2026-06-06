dc25c040a993b3be6d4da3f0990c744a4ab0b5fd
Naoto Kaneko <naoty.k@gmail.com>
2026-06-06 10:09:04 +0900

    駒を指したあとの局面を生成する

--- packages/engine/src/play.test.ts
+++ packages/engine/src/play.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest";
-import { initialBoard } from "./board";
+import { boardWith, initialBoard } from "./board";
 import { applyPlay } from "./play";
 import { setupHands } from "./test-data";
 
@@ -14,5 +14,39 @@ describe("applyPlay", () => {
     const play2 = { type: "move" as const, from: "33" as const, to: "34" as const, promote: false };
     const position3 = applyPlay(position2, play2);
     expect(position3.turn).toBe("black");
+  });
+
+  test("駒を指す手の場合、移動元のマスを空にし、移動先のマスに駒を置く", () => {
+    const board = boardWith({
+      "28": { color: "black", type: "rook" },
+      "24": { color: "white", type: "pawn" },
+    });
+    const hands = setupHands();
+    const position1 = { board, hands, turn: "black" as const };
+    const play = { type: "move" as const, from: "28" as const, to: "24" as const, promote: false };
+    const position2 = applyPlay(position1, play);
+    expect(position2.board["28"]).toBeNull();
+    expect(position2.board["24"]).toEqual({ color: "black", type: "rook" });
+  });
+
+  test("駒を指す手が成る場合、移動先のマスに成った駒を置く", () => {
+    const board = boardWith({
+      "88": { color: "black", type: "bishop" },
+      "22": { color: "white", type: "bishop" },
+    });
+    const hands = setupHands();
+    const position1 = { board, hands, turn: "black" as const };
+    const play = { type: "move" as const, from: "88" as const, to: "22" as const, promote: true };
+    const position2 = applyPlay(position1, play);
+    expect(position2.board["88"]).toBeNull();
+    expect(position2.board["22"]).toEqual({ color: "black", type: "bishop+" });
+  });
+
+  test("駒を指す手で、移動元のマスに駒がない場合、エラーを投げる", () => {
+    const board = boardWith();
+    const hands = setupHands();
+    const position = { board, hands, turn: "black" as const };
+    const play = { type: "move" as const, from: "88" as const, to: "22" as const, promote: false };
+    expect(() => applyPlay(position, play)).toThrow("illegal play: no piece at 88");
   });
 });
--- packages/engine/src/play.ts
+++ packages/engine/src/play.ts
@@ -1,9 +1,49 @@
-import type { Play, Position } from "./types";
+import type { PieceType, Play, Position } from "./types";
 
-export function applyPlay(position: Position, _play: Play): Position {
-  return {
-    board: position.board,
-    hands: position.hands,
-    turn: position.turn === "black" ? "white" : "black",
-  };
+export function applyPlay(position: Position, play: Play): Position {
+  switch (play.type) {
+    case "move": {
+      const piece = position.board[play.from];
+      if (piece === null) {
+        throw new Error(`illegal play: no piece at ${play.from}`);
+      }
+
+      const newBoard = {
+        ...position.board,
+        [play.from]: null,
+        [play.to]: play.promote ? { type: promote(piece.type), color: piece.color } : piece,
+      };
+
+      return {
+        ...position,
+        board: newBoard,
+        turn: position.turn === "black" ? "white" : "black",
+      };
+    }
+    default: {
+      return {
+        ...position,
+        turn: position.turn === "black" ? "white" : "black",
+      };
+    }
+  }
+}
+
+function promote(pieceType: PieceType): PieceType {
+  switch (pieceType) {
+    case "pawn":
+      return "pawn+";
+    case "lance":
+      return "lance+";
+    case "knight":
+      return "knight+";
+    case "silver":
+      return "silver+";
+    case "bishop":
+      return "bishop+";
+    case "rook":
+      return "rook+";
+    default:
+      return pieceType;
+  }
 }
