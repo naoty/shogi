d357b88d49ccbe794b85b9ba0fb254a5502c5b9b
Naoto Kaneko <naoty.k@gmail.com>
2026-06-21 22:21:22 +0900

    関数や型の一部をパッケージ外に公開する

---
+++ packages/engine/src/index.ts
@@ -0,0 +1,20 @@
+export { isCheck } from "./check";
+export { isCheckmate } from "./checkmate";
+export { legalPlaysOf } from "./legal-plays";
+export { applyPlay } from "./play";
+
+export type {
+  Board,
+  Color,
+  Column,
+  Drop,
+  Hand,
+  Hands,
+  Move,
+  Piece,
+  PieceType,
+  Play,
+  Position,
+  Row,
+  Square,
+} from "./types";
